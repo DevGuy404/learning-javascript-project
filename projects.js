ca18e2b034c09554e9ba56a7b3851e7906ba382e
Devguy404 <arif.edu007@gmail.com>
2023-04-26 10:53:57 +0600

    setup nodejs for spine matchine project.

---
+++ projects.js
@@ -0,0 +1,7 @@
+// 1. Despot some money
+// 2. Determine number of lines to bet on
+// 3. Collect a bat amount
+// 4. spin the machine
+// 5. check if the user won
+// 6. give the user their winings
+// 7. play again
